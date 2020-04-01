02cb5af27db1f7e5cfb672184dff8d5c0a06d76f
Yana Preobrazhenskaya <yanak.preobrazhenskaya@yandex.ru>
2020-04-01 21:37:18 +0400

    Create User model

---
+++ BankSystem/JDBC/src/main/java/Models/User.java
@@ -0,0 +1,61 @@
+package Models;
+
+import java.util.UUID;
+
+public class User {
+    private UUID id;
+    private String login;
+    private String password;
+    private String address;
+    private String phone;
+
+    public User () { }
+
+    public User(UUID id, String login, String password, String address, String phone) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
