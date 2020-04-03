3c30588eb537961cc34bef9abe653432af7fd687
Yana Preobrazhenskaya <yanak.preobrazhenskaya@yandex.ru>
2020-04-03 17:54:32 +0400

    Base for Spring app

---
+++ BankSystem/BankSystemSpring/src/main/java/ssu/BankSystemSpring/BankSystemSpringApplication.java
@@ -0,0 +1,13 @@
+package ssu.BankSystemSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSystemSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSystemSpringApplication.class, args);
+	}
+
+}
